0a8dc6166700ccf0d0e7c28c66235eadcb695ae7
Valerio Santinelli <santinelli@altralogica.it>
2024-06-07 15:10:46 +0200

    Better docker script

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -1,1 +1,3 @@
-docker buildx build --platform linux/amd64 -t tanis2000/discomfort:latest-linux-amd64 --push -f ./deploy/docker/Dockerfile .
+export BUILD_VERSION="0.1.0"
+
+docker buildx build --platform linux/amd64,linux/arm64 -t tanis2000/discomfort:${BUILD_VERSION} -t tanis2000/discomfort --build-arg BUILD_VERSION=${BUILD_VERSION} --push -f ./deploy/docker/Dockerfile .
