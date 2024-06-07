1b9b81360412e7d41d785ae5b3aad6fa2cd4e7b2
Valerio Santinelli <santinelli@altralogica.it>
2024-06-07 15:27:28 +0200

    Better deployment

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -1,3 +1,4 @@
-export BUILD_VERSION="0.1.0"
+BUILD_VERSION=$(git describe --tags HEAD)
+export BUILD_VERSION
 
-docker buildx build --platform linux/amd64,linux/arm64 -t tanis2000/discomfort:${BUILD_VERSION} -t tanis2000/discomfort --build-arg BUILD_VERSION=${BUILD_VERSION} --push -f ./deploy/docker/Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 -t tanis2000/discomfort:"${BUILD_VERSION}" -t tanis2000/discomfort --build-arg BUILD_VERSION="${BUILD_VERSION}" --push -f ./deploy/docker/Dockerfile .
